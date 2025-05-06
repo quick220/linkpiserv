c870595b6b40bcd497e5fab8d05d41330a2705da
zc <zc@linkpi.cn>
2025-05-06 16:01:32 +0800

    20250506

--- shell/init.sh
+++ shell/init.sh
@@ -1,3 +1,5 @@
+sysctl -w net.core.rmem_max=5000000
+sysctl -w net.core.rmem_default=5000000
 /link/shell/loop.sh /link/bin/Intercom &
 /link/shell/loop.sh /link/shell/sls.sh &
 /link/bin/frps -c /link/config/frps.ini &
